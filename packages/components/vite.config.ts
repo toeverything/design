6865c2df8aff766a859ec27f51e380058b922824
QiShaoXuan <qishaoxuan777@gmail.com>
2023-07-27 18:33:42 +0800

    feat: modify build step

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,28 +1,39 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'node:path';
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-import pkg from './package.json';
+import { vanillaExtractPlugin as vanillaExtractVitePlugin } from '@vanilla-extract/vite-plugin';
 import { fileURLToPath } from 'url';
-
+import { vanillaExtractPlugin as vanillaExtractRollupPlugin } from '@vanilla-extract/rollup-plugin';
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
+const isBuild = process.env.NODE_ENV === 'build';
+
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [react(), isBuild ? null : vanillaExtractVitePlugin()],
   build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      formats: ['es'],
-    },
-    sourcemap: true,
+    target: 'modules',
+    //打包文件目录
+    outDir: 'dist',
+    //压缩
+    minify: false,
+    cssCodeSplit: true,
     rollupOptions: {
       external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'react-dom',
+      input: [resolve(__dirname, 'src/index.ts')],
+      plugins: [vanillaExtractRollupPlugin()],
+
+      output: [
+        {
+          format: 'es',
+          entryFileNames: '[name].js',
+          preserveModules: true,
+          dir: 'dist',
+          preserveModulesRoot: 'src',
         },
-      },
+      ],
+    },
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
     },
   },
 });
