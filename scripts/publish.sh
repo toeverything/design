ff4b581e50d2ecce188a3ee077e29bc0ddc4261e
QiShaoXuan <qishaoxuan777@gmail.com>
2023-07-27 18:28:05 +0800

    feat: modify github workflow

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-packages=(
-  "theme"
-)
-
 npm config set '//registry.npmjs.org/:_authToken' "${NPM_TOKEN}"
 
-for package in "${packages[@]}"; do
-  cd "packages/$package"
-  pnpm run build
+cd "packages/$PACKAGE_NAME"
+pnpm run build
 
-  if [ "$NIGHTLY" = "true" ]; then
-    pnpm publish --no-git-checks --tag nightly
-  else
-    pnpm publish
-  fi
+if [ "$NIGHTLY" = "true" ]; then
+  pnpm publish --no-git-checks --tag nightly
+else
+  pnpm publish
+fi
 
-  cd ../../
-done
+cd ../../
