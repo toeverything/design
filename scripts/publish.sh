ace8a7d99e361a12bc22ccfc6b69e0ac22afde0e
QiShaoXuan <qishaoxuan777@gmail.com>
2023-08-02 17:36:43 +0800

    feat: modify release workflow

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,10 +5,12 @@ npm config set '//registry.npmjs.org/:_authToken' "${NPM_TOKEN}"
 cd "packages/$PACKAGE_NAME"
 pnpm run build
 
-if [ "$NIGHTLY" = "true" ]; then
-  pnpm publish --no-git-checks --tag nightly
-else
-  pnpm publish
-fi
+pnpm version $VERSION_TYPE
+git config --global user.email "474021214@qq.com"
+git config --global user.name "QiShaoXuan"
+git add .;
+git commit -m "bump the $VERSION_TYPE version" --no-verify
+git push
+pnpm publish
 
 cd ../../
