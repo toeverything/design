3f188d88c27b1243c8746614b52ab2c55df2f448
QiShaoXuan <qishaoxuan777@gmail.com>
2023-08-03 11:09:39 +0800

    fix: can not bump version automaticaly

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,12 +5,6 @@ npm config set '//registry.npmjs.org/:_authToken' "${NPM_TOKEN}"
 cd "packages/$PACKAGE_NAME"
 pnpm run build
 
-pnpm version $VERSION_TYPE
-
-git add .
-git commit -m "bump the $VERSION_TYPE version" --no-verify
-git push
-
 pnpm publish
 
 cd ../../
