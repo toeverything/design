04a473755fd8291f9a87a84b94a5f994be14488c
JimmFly <yangjinfei001@gmail.com>
2023-08-18 11:50:24 +0800

    feat: add new properties to divider

--- packages/components/src/divider/style.css.ts
+++ packages/components/src/divider/style.css.ts
@@ -2,15 +2,20 @@ import { style } from '@vanilla-extract/css';
 
 export const divider = style({
   height: '1px',
-  backgroundColor: 'var(--affine-divider-color)',
+  backgroundColor:'var(--affine-divider-color)',
   borderRadius: '8px',
   margin: '8px 0',
   width: '100%',
 });
+export const thinner = style({
+  height: '0.5px',
+});
 export const verticalDivider = style({
   width: '1px',
-  backgroundColor: 'var(--affine-divider-color)',
   borderRadius: '8px',
   height: '100%',
   margin: '0 2px',
+});
+export const verticalThinner = style({
+  width: '0.5px',
 });
