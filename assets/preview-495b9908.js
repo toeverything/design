1e7eaa955c8b2c7543cf98e31a24b592a0ac2cb7
QiShaoXuan <474021214@qq.com>
2023-08-18 08:45:13 +0000

    Deploying to gh-pages from @ toeverything/design@35aedfc930aa9e33eb6aa7bd6937ac817d68462e 🚀

---
+++ assets/preview-495b9908.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-9b923889.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-515b37ad.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-515b37ad.js","./iframe-9b923889.js","./index-61bf1805.js","./_commonjsHelpers-de833af9.js","./_getPrototype-9f29b529.js","./index-2801d3c9.js","./index-d475d2ea.js","./index-d37d4223.js","./index-de0143ae.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-495b9908.js.map
