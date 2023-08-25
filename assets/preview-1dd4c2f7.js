e14d53cc0e64a2165c1b4c47f88a2ee81743ab51
himself65 <himself65@users.noreply.github.com>
2023-08-25 01:27:56 +0000

    Deploying to gh-pages from @ toeverything/design@5142dc26a04b99abb58a44c012010daecc42519d 🚀

--- assets/preview-1dd4c2f7.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-f8882ecc.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-796e51d2.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-796e51d2.js","./iframe-f8882ecc.js","./index-61bf1805.js","./_commonjsHelpers-de833af9.js","./_getPrototype-9f29b529.js","./index-2801d3c9.js","./index-d475d2ea.js","./index-d37d4223.js","./index-de0143ae.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-1dd4c2f7.js.map
