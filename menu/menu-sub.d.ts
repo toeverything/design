45bcff3d71013b5c5a603468070d5e5cd690220b
QiShaoXuan <474021214@qq.com>
2023-08-29 13:51:50 +0000

    Deploying to gh-pages from @ toeverything/design@2f509be9e81495171aad43a807b3abfcfa482348 🚀

--- menu/menu-sub.d.ts
+++ menu/menu-sub.d.ts
@@ -3,10 +3,10 @@ import type { ReactNode } from 'react';
 import type { MenuItemProps } from './menu-item.tsx';
 export interface MenuSubProps {
     children: ReactNode;
-    items: ReactNode[];
+    items: ReactNode;
     triggerOptions?: Omit<MenuItemProps, 'onSelect' | 'children'>;
     portalOptions?: Omit<MenuPortalProps, 'children'>;
     subOptions?: Omit<DropdownMenuSubProps, 'children'>;
     subContentOptions?: Omit<MenuSubContentProps, 'children'>;
 }
-export declare const MenuSub: ({ children: propsChildren, items, portalOptions, subOptions, triggerOptions: { className: propsClassName, preFix, endFix, type }, subContentOptions: { className: subContentClassName, ...otherSubContentOptions }, }: MenuSubProps) => import("react/jsx-runtime").JSX.Element;
+export declare const MenuSub: ({ children: propsChildren, items, portalOptions, subOptions, triggerOptions: { className: propsClassName, preFix, endFix, type, ...otherTriggerOptions }, subContentOptions: { className: subContentClassName, ...otherSubContentOptions }, }: MenuSubProps) => import("react/jsx-runtime").JSX.Element;
