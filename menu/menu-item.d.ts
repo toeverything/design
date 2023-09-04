de718fc77584dfe4a7d5135bd07b1b2b36fa96c7
QiShaoXuan <474021214@qq.com>
2023-09-04 10:17:00 +0000

    Deploying to gh-pages from @ toeverything/design@7dc8fca209c738eb4cc7adcb54c712f5b0664326 🚀

--- menu/menu-item.d.ts
+++ menu/menu-item.d.ts
@@ -1,5 +1,5 @@
-/// <reference types="react" />
 import type { MenuItemProps as MenuItemPropsPrimitive } from '@radix-ui/react-dropdown-menu';
+import { type HTMLAttributes } from 'react';
 export interface MenuItemProps extends Omit<MenuItemPropsPrimitive, 'asChild' | 'textValue'> {
     type?: 'default' | 'warning' | 'danger';
     preFix?: React.ReactNode;
@@ -7,4 +7,4 @@ export interface MenuItemProps extends Omit<MenuItemPropsPrimitive, 'asChild' | 
     checked?: boolean;
     selected?: boolean;
 }
-export declare const MenuItem: ({ children: propsChildren, type, className: propsClassName, preFix, endFix, checked, selected, ...otherProps }: MenuItemProps) => import("react/jsx-runtime").JSX.Element;
+export declare const MenuItem: import("react").ForwardRefExoticComponent<MenuItemProps & HTMLAttributes<HTMLDivElement> & import("react").RefAttributes<HTMLDivElement>>;
--- menu/menu-sub.d.ts
+++ menu/menu-sub.d.ts
@@ -1,12 +1,13 @@
 import type { DropdownMenuSubProps, MenuPortalProps, MenuSubContentProps } from '@radix-ui/react-dropdown-menu';
 import type { ReactNode } from 'react';
+import { type HTMLAttributes } from 'react';
 import type { MenuItemProps } from './menu-item.tsx';
 export interface MenuSubProps {
     children: ReactNode;
     items: ReactNode;
-    triggerOptions?: Omit<MenuItemProps, 'onSelect' | 'children'>;
+    triggerOptions?: Omit<MenuItemProps, 'onSelect' | 'children'> & MenuItemProps & HTMLAttributes<HTMLDivElement>;
     portalOptions?: Omit<MenuPortalProps, 'children'>;
     subOptions?: Omit<DropdownMenuSubProps, 'children'>;
     subContentOptions?: Omit<MenuSubContentProps, 'children'>;
 }
-export declare const MenuSub: ({ children: propsChildren, items, portalOptions, subOptions, triggerOptions: { className: propsClassName, preFix, endFix, type, ...otherTriggerOptions }, subContentOptions: { className: subContentClassName, ...otherSubContentOptions }, }: MenuSubProps) => import("react/jsx-runtime").JSX.Element;
+export declare const MenuSub: import("react").ForwardRefExoticComponent<MenuSubProps & import("react").RefAttributes<HTMLDivElement>>;
