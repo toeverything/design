7dc8fca209c738eb4cc7adcb54c712f5b0664326
QiShaoXuan <qishaoxuan777@gmail.com>
2023-09-04 18:15:37 +0800

    feat: add forwardRef to menu trigger

--- packages/components/src/menu/menu-item.tsx
+++ packages/components/src/menu/menu-item.tsx
@@ -1,5 +1,6 @@
 import type { MenuItemProps as MenuItemPropsPrimitive } from '@radix-ui/react-dropdown-menu';
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+import { forwardRef, type HTMLAttributes } from 'react';
 
 import { useMenuItem } from './use-menu-item.tsx';
 
@@ -12,29 +13,38 @@ export interface MenuItemProps
   selected?: boolean;
 }
 
-export const MenuItem = ({
-  children: propsChildren,
-  type = 'default',
-  className: propsClassName,
-  preFix,
-  endFix,
-  checked,
-  selected,
-  ...otherProps
-}: MenuItemProps) => {
-  const { className, children } = useMenuItem({
-    children: propsChildren,
-    className: propsClassName,
-    type,
-    preFix,
-    endFix,
-    checked,
-    selected,
-  });
+export const MenuItem = forwardRef<
+  HTMLDivElement,
+  MenuItemProps & HTMLAttributes<HTMLDivElement>
+>(
+  (
+    {
+      children: propsChildren,
+      type = 'default',
+      className: propsClassName,
+      preFix,
+      endFix,
+      checked,
+      selected,
+      ...otherProps
+    },
+    ref
+  ) => {
+    const { className, children } = useMenuItem({
+      children: propsChildren,
+      className: propsClassName,
+      type,
+      preFix,
+      endFix,
+      checked,
+      selected,
+    });
 
-  return (
-    <DropdownMenu.Item className={className} {...otherProps}>
-      {children}
-    </DropdownMenu.Item>
-  );
-};
+    return (
+      <DropdownMenu.Item ref={ref} className={className} {...otherProps}>
+        {children}
+      </DropdownMenu.Item>
+    );
+  }
+);
+MenuItem.displayName = 'MenuItem';
