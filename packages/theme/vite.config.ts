a792102f1354ad05c8d7c010e5a5b57187caccb5
Peng Xiao <pengxiao@outlook.com>
2024-11-21 14:13:47 +0800

    feat: bundle dts file

--- packages/theme/vite.config.ts
+++ packages/theme/vite.config.ts
@@ -1,6 +1,8 @@
+import fs from 'node:fs';
 import { resolve } from 'node:path';
 
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import { generateDtsBundle } from 'dts-bundle-generator';
 import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
@@ -18,10 +20,34 @@ export default defineConfig({
         index: resolve(__dirname, 'src/index.ts'),
         css: resolve(__dirname, 'src/index.css.ts'),
         'v2/index': resolve(__dirname, 'src/v2/index.ts'),
-        'presets/typography': resolve(__dirname, 'src/presets/typography.css.ts'),
+        'presets/typography': resolve(
+          __dirname,
+          'src/presets/typography.css.ts'
+        ),
       },
       name: 'ToEverythingTheme',
     },
   },
-  plugins: [dts(), vanillaExtractPlugin()],
+  plugins: [
+    dts({
+      afterBuild() {
+        const inputs = [
+          resolve(__dirname, 'dist/index.d.ts'),
+          resolve(__dirname, 'dist/v2/index.d.ts'),
+        ];
+
+        const dtsFiles = generateDtsBundle(
+          inputs.map(filePath => ({ filePath })),
+          {
+            preferredConfigPath: resolve(__dirname, 'tsconfig.json'),
+          }
+        );
+        // write back to source
+        dtsFiles.forEach((file, index) => {
+          fs.writeFileSync(inputs[index], file, 'utf-8');
+        });
+      },
+    }),
+    vanillaExtractPlugin(),
+  ],
 });
